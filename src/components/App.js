7236e785ebfa07b545ccfbcd426c6f62203c8517
tyesc <55241427+tyesc@users.noreply.github.com>
2020-01-07 14:45:12 +0100

    feat: add from for search movie or tv show

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+import logo from '../logo.svg';
+import '../styles/App.css';
+import SearchFormMovie from './SearchFormMovie';
+import SearchFormTv from './SearchFormTv';
+
+function App() {
+
+  return (<SearchFormTv />)
+}
+
+export default App;
---
+++ src/components/SearchFormMovie.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form } from 'react-bootstrap';
+
+const url = 'https://api.themoviedb.org/3/search/movie?api_key=18cb3ed1e51594213b505970b2c9a0bf&language=en-US&';
+
+
+export default () => {
+  const [state, setState] = useState({
+    get: [],
+    query: 'willow',
+    page: 1,
+  })
+  useEffect(() => {
+    search(state.query, state.page);
+  }, [state.page, state.query]);
+
+  const search = (query, page) => {
+
+    if (state.query !== undefined) {
+      axios.get(`${url}query=${query}&page=${page}&include_adult=false`)
+        .then(response => {
+          setState({ get: response.data.results });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+
+  return (
+
+    <>
+      <Form.Control type="text"
+        placeholder="Normal text"
+        onChange={e => setState({
+          ...state,
+          query: e.target.value
+         })}
+      />
+      <ul>
+        {
+          state.get.map((elem, index) => (
+            <li key={index}>{elem.title}</li>
+          ))
+        }
+      </ul>
+    </>
+
+  );
+}
