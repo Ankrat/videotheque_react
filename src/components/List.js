ec8177727ef0dc72225196c54da1d730c4795409
tyesc <55241427+tyesc@users.noreply.github.com>
2020-01-27 11:20:42 +0100

    feat: add btn for login or disconnect

--- src/components/List.js
+++ src/components/List.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { Tabs, Tab } from '@poool/junipero';
+import { Tabs, Tab, Button } from '@poool/junipero';
 
 import '../styles/List.css';
 import WatchListMovie from './watchlist/WatchListMovie';
 import WatchListTv from './watchlist/WatchListTv';
+
+const AuthStr = sessionStorage.getItem('Authorization');
 
 export default () => {
 
   return (
     <>
       <h1>WatchList</h1>
+      { AuthStr ?
+        ( <Button
+          className="redirect-log-btn"
+          reversed={true}
+          type="danger"
+          onClick={() => {
+            sessionStorage.clear();
+            window.location = '/';
+          }}
+        >Disconnect
+        </Button> ) :
+        ( <Button
+          className="redirect-log-btn"
+          type="primary"
+          onClick={() => window.location = '/'}
+        >Login
+        </Button> )
+      }
       <Tabs>
         <Tab title="Movie"><WatchListMovie /></Tab>
         <Tab title="Tv"><WatchListTv /></Tab>
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { Tabs, Tab } from '@poool/junipero';
+import { Tabs, Tab, Button } from '@poool/junipero';
 
 import SearchFormMovie from './SearchFormMovie';
 import SearchFormTv from './SearchFormTv';
 
 import '../../styles/Search.css';
 
+const AuthStr = sessionStorage.getItem('Authorization');
+
 export default () => {
   return (
     <>
+      <div>
+        <h1>Search</h1>
+        { AuthStr ?
+          ( <Button
+            className="redirect-log-btn"
+            reversed={true}
+            type="danger"
+            onClick={() => {
+              sessionStorage.clear();
+              window.location = '/';
+            }}
+          >Disconnect
+          </Button> ) :
+          ( <Button
+            className="redirect-log-btn"
+            type="primary"
+            onClick={() => window.location = '/'}
+          >Login
+          </Button> )
+        }
+      </div>
       <Tabs>
         <Tab title="Movie"><SearchFormMovie /></Tab>
         <Tab title="Tv"><SearchFormTv /></Tab>
