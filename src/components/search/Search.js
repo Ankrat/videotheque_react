9f9ce0b6f2ea46caab579ee1da8034853a5b8b8a
tyesc <55241427+tyesc@users.noreply.github.com>
2020-01-28 11:52:59 +0100

    feat: add menu user

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Tabs, Tab, Button } from '@poool/junipero';
+import {
+  Tabs, Tab, Button,
+  Dropdown, DropdownToggle, DropdownMenu,
+  DropdownItem, Toggle } from '@poool/junipero';
+import { Image } from 'react-bootstrap';
 
 import SearchFormMovie from './SearchFormMovie';
 import SearchFormTv from './SearchFormTv';
 
 import '../../styles/Search.css';
+import userImg from '../../styles/img/userImg.png';
 
 const AuthStr = sessionStorage.getItem('Authorization');
 const userName = sessionStorage.getItem('userName');
+// const userImg = sessionStorage.getItem('userImg');
 
 export default () => {
   return (
@@ -17,16 +23,49 @@ export default () => {
         { AuthStr ?
           (
             <div className="redirect-log-btn">
-              <p>{ userName }</p>
-              <Button
-                reversed={true}
-                type="danger"
-                onClick={() => {
-                  sessionStorage.clear();
-                  window.location = '/';
-                }}
-              >Disconnect
-              </Button>
+              <Dropdown theme="none" placement="bottom-end">
+                <DropdownToggle>
+                  <Button theme="none" type="primary" className="log-btn">
+                    <Image
+                      src={ `${userImg}` }
+                      className="user-img"
+                      roundedCircle/>
+                    <p>{ userName }</p>
+                  </Button>
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem><a>Parameter</a></DropdownItem>
+                  <DropdownItem>
+                    <Toggle
+                      className="logout-btn"
+                      theme="none"
+                      uncheckedLabel="En"
+                      checkedLabel="Fr"
+                    />
+                  </DropdownItem>
+                  <DropdownItem>
+                    <Toggle
+                      className="logout-btn"
+                      theme="none"
+                      uncheckedLabel="Dark mode"
+                      checkedLabel="Dark mode"
+                    />
+                  </DropdownItem>
+                  <DropdownItem
+                    className="logout-btn"
+                  >
+                    <Button
+                      reversed={true}
+                      type="danger"
+                      onClick={() => {
+                        sessionStorage.clear();
+                        window.location = '/';
+                      }}
+                    >Disconnect
+                    </Button>
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
             </div>
           ) :
           (
