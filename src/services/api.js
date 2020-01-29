bba773df9d52f10c02eb09fe1d3ca29ae4dc997e
tyesc <55241427+tyesc@users.noreply.github.com>
2020-01-29 15:03:36 +0100

    style: state of view

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const AuthStr = sessionStorage.getItem('Authorization');
+const userId = sessionStorage.getItem('userId');
+const burl = 'http://localhost:8085/api';
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': AuthStr,
+};
+
+
+export default {
+
+  updateState: () => {
+    return axios.put;
+  },
+
+};
