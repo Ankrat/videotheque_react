4403aa24489074279df4b28bd789c9e96cef2253
tyesc <55241427+tyesc@users.noreply.github.com>
2020-02-14 16:21:28 +0100

    refactor: clean code

--- src/components/search/TvDetails.js
+++ src/components/search/TvDetails.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Image,
   Collapse, Accordion, Card } from 'react-bootstrap';
