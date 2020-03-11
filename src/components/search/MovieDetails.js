99301c441b982567be577a5333843b6e89e7c0ca
tyesc <55241427+tyesc@users.noreply.github.com>
2020-03-11 15:17:00 +0100

    feat: add button if user is connected

--- src/components/search/MovieDetails.js
+++ src/components/search/MovieDetails.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 
-
 import '../../styles/Details.css';
 import API from '../../services/api';
 import ButtonAdd from '../fragments/ButtonAdd';
@@ -24,7 +23,6 @@ export default (props) => {
   }, [props.match.params.id]);
 
   return (
-
     <div
       className="background"
       style={{backgroundImage:
@@ -40,31 +38,52 @@ export default (props) => {
             />
           </Col>
           <Col>
-            <ButtonAdd
-              title="Add to WatchList"
-              className="btn-add"
-              reversed={true}
-              type="success"
-              url={urlApi(userId).movie}
-              data={{
-                poster_path: state.get.poster_path,
-                title: state.get.title,
-                id_details: state.get.id,
-              }}
-            />
+            {
+              userId
+                ? (
+                  <ButtonAdd
+                    title="Add to WatchList"
+                    className="btn-add"
+                    reversed={true}
+                    type="success"
+                    url={urlApi(userId).movie}
+                    data={{
+                      poster_path: state.get.poster_path,
+                      title: state.get.title,
+                      id_details: state.get.id,
+                    }}
+                  />
+                ) : ''
+            }
           </Col>
         </Row>
-        <h2>{state.get.original_title}</h2>
-        <p>{state.get.release_date} / {state.get.runtime} min / {
-          state.genres.map((elem, index) => (
-            elem.name + ' '
-          ))
-        }</p>
-        <p>{state.country.map((elem, index) => (
-          elem.name
-        ))}</p>
-        <h2>SYNOPSIS ET DÉTAILS</h2>
-        <p>{state.get.overview}</p>
+        <div className="details-content">
+          <h2>
+            {
+              state.get.original_title
+            }{
+              state.get.belongs_to_collection
+                ? ` (${state.get.belongs_to_collection.name})`
+                : ''
+            }
+          </h2>
+          <p><span>Release date:</span> {state.get.release_date}</p>
+          <p><span>Runtime:</span> {state.get.runtime} min</p>
+          <p><span>Genres:</span> {
+            state.genres.map((elem, index) => (
+              elem.name + ' / '
+            ))
+          }
+          </p>
+          <p><span>Country:</span> {
+            state.country.map((elem, index) => (
+              elem.name
+            ))
+          }
+          </p>
+          <h2>Synopsis & Details</h2>
+          <p>{state.get.overview}</p>
+        </div>
       </Container>
     </div>
 
